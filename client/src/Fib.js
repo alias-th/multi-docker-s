9c09b961a12e0f9cb480b386f69758f00324e379
Jame Monton <monton.onnom1@gmail.com>
2022-03-11 19:22:26 +0700

    added new client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,11 +1,11 @@
-import { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: "",
+    index: '',
   };
 
   componentDidMount() {
@@ -14,28 +14,28 @@ class Fib extends Component {
   }
 
   async fetchValues() {
-    const values = await axios.get("/api/values/current");
+    const values = await axios.get('/api/values/current');
     this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get("/api/values/all");
+    const seenIndexes = await axios.get('/api/values/all');
     this.setState({
       seenIndexes: seenIndexes.data,
     });
   }
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post("/api/values", {
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
       index: this.state.index,
     });
-
-    this.setState({ index: "" });
+    this.setState({ index: '' });
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(", ");
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
@@ -48,6 +48,7 @@ class Fib extends Component {
         </div>
       );
     }
+
     return entries;
   }
 
@@ -58,7 +59,7 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={(e) => this.setState({ index: e.target.value })}
+            onChange={(event) => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
